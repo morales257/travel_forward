05528b0f545bf7010ddaa5f143a782d87428ed0e
Luis Morales <davidmorales257@gmail.com>
2016-09-29 23:11:31 -0400

    syntax fit

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,7 +7,7 @@ class Page < ActiveRecord::Base
                     :s3_credentials => {
                       :access_key_id => ENV['S3_ACCESS_KEY'],
                       :secret_access_key => ENV['S3_SECRET_KEY'],
-                      :bucket: ENV['S3_BUCKET'] },
+                      :bucket => ENV['S3_BUCKET'] },
                     :s3_permissions => :public_read,
                     styles: { medium: "300x300>", resize: "50%"}
   validates_attachment_content_type :background, content_type: /\Aimage/
