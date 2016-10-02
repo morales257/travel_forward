9e0606fdc01634c1dc8abac221c243f41311d6b1
Luis Morales <davidmorales257@gmail.com>
2016-10-02 14:01:13 -0400

    Add host name

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,6 +4,7 @@ class Page < ActiveRecord::Base
   validates :background, presence: true
   has_attached_file :background,
                     :storage =>:s3,
+                    :s3_host_name => 's3-us-west-2.amazonaws.com',
                     :s3_credentials => {
                       :access_key_id => ENV['S3_ACCESS_KEY'],
                       :secret_access_key => ENV['S3_SECRET_KEY']
