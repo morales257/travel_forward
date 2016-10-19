3baa38705bcbf7078544a635208faac6f77a44e3
Luis Morales <davidmorales257@gmail.com>
2016-10-19 09:18:30 -0400

    test

--- app/preview_pdfs/preview_pdf.rb
+++ app/preview_pdfs/preview_pdf.rb
@@ -1,10 +1,12 @@
+require 'open-uri'
 class PreviewPDF < Prawn::Document
 
   def initialize(itinerary)
     #run initialize method from the super class
     super()
     @itin_preview = itinerary
-    reader = PDF::Reader.new("public/#{@itin_preview.document_file_name}")
+    io = open('https://s3-us-west-2.amazonaws.com/travelforwardapp/itineraries/documents/000/000/007/original/Joe_Wehinger_Report_-_Week_2.pdf')
+    reader = PDF::Reader.new(io)
     formatted_page = reader.page(1)
     formatted_text = formatted_page.text.gsub! /\s+/, ''
     page_text = formatted_text.gsub!(/$/, "\n")
@@ -12,6 +14,6 @@ class PreviewPDF < Prawn::Document
 
     #text "#{content}"
     text page_text, :align => :left
-
   end
+
 end
