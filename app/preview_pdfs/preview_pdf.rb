bbb00ccec4af41453a0081dae902464d2b8532db
Luis Morales <davidmorales257@gmail.com>
2016-10-19 10:05:10 -0400

    test asset path

--- app/preview_pdfs/preview_pdf.rb
+++ app/preview_pdfs/preview_pdf.rb
@@ -5,7 +5,7 @@ class PreviewPDF < Prawn::Document
     #run initialize method from the super class
     super()
     @itin_preview = itinerary
-    io = open('https://s3-us-west-2.amazonaws.com/travelforwardapp/itineraries/documents/000/000/007/original/Joe_Wehinger_Report_-_Week_2.pdf')
+    io = open(APP_CONFIG[Rails.env]['itinerary_path'])
     reader = PDF::Reader.new(io)
     formatted_page = reader.page(1)
     formatted_text = formatted_page.text.gsub! /\s+/, ''
